35ebee1d5952a409c978bd5a220aeb7f04a2c1b5
LuzIreneSeedTech <cortez.luzirene@seedtech.ph>
2024-04-02 14:04:31 +0800

    added confirmable functionality upon user sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_013928) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_02_054439) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -52,6 +52,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_02_013928) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
