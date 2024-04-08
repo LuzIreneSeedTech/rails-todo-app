40c41c02d0739bbe211826ff3c48a1f218639c4c
LuzIreneSeedTech <cortez.luzirene@seedtech.ph>
2024-04-08 14:53:57 +0800

    basic layout of authenticated user homepage

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,6 @@ class UserController < ApplicationController
     before_action :authenticate_user!
 
     def index
-      
+      @user_categories = User.all
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_one_attached :image do |attachable|
+    attachable.variant :thumb, resize_to_fill: [60, 60, crop: :attention] 
+  end
 end
